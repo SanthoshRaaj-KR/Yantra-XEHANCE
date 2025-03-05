b86dee27e2bf54bc0b7adbaf067d0a307c34558f
Santhosh <santhosh.kr17@gmail.com>
2025-03-05 20:31:42 +0530

    Addedd components for all parts of the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
+import About from "./components/About";
+import Suggestions from "./components/Suggestions";
+import Enhance from './components/Enhance';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // const [count, setCount] = useState(0);
+  const handleButton = (e) => {
+    console.log(e)
+    console.log("Hello");
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     
+      <div class="min-w-screen min-h-screen relative overflow-auto scroll-smooth" >
+        <div class="absolute inset-0  md:bg-[url('./assets/cover.jpg')] bg-cover bg-center bg-no-repeat mt-6 overflow-x-clip" id="home">
+          <NavBar />
+          <Hero />
+          <Demo />
+
+          <div
+            class="container bg-stone-300 absolute min-w-screen  "
+            id="about"
+          >
+            <div class="mt-20">
+              <About />
+            </div>
+            <div class=" flex min-w-screen min-h-screen bg-white justify-center items-center ">
+              <Enhance />
+              
+              
+            </div>
+            <Suggestions />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+     
+   
+  );
 }
 
-export default App
+export default App;
